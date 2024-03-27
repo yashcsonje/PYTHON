8f44a52f0cddf5cff7359a67dbe6c6f42c834423
yashcsonje <162028752+yashcsonje@users.noreply.github.com>
2024-03-27 23:38:48 +0530

    Add files via upload

---
+++ OOPs/Default_Constructor.py
@@ -0,0 +1,8 @@
+#When the constructor doesn't accept any arguments from the object and has only one argument, self, in the constructor, it is known as a Default constructor.
+
+class Details:
+  def __init__(self):
+    print("animal Crab belongs to Crustaceans group")
+obj1=Details()
+
+#Output: animal Crab belongs to Crustaceans group
