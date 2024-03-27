559cf483f0694007919a03e8bc6809a647c1b5f1
yashcsonje <162028752+yashcsonje@users.noreply.github.com>
2024-03-27 19:30:06 +0530

    Update Class_and_Objects.py

--- OOPs/Class_and_Objects.py
+++ OOPs/Class_and_Objects.py
@@ -8,17 +8,3 @@ print(obj1.age)
 #Output 
 Rohan
 20
-
-#Self Parameter
-class Details:
-    name = "Rohan"
-    age = 20
-
-    def desc(self):
-        print("My name is", self.name, "and I'm", self.age, "years old.")
-
-obj1 = Details()
-obj1.desc()
-
-#Output 
-My name is Rohan and I'm 20 years old.
