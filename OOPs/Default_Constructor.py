393ca81c69eea889ff4a561c1889be2e010c0ce0
yashcsonje <162028752+yashcsonje@users.noreply.github.com>
2024-03-30 09:43:37 +0530

    Update Default_Constructor.py

--- OOPs/Default_Constructor.py
+++ OOPs/Default_Constructor.py
@@ -1,3 +1,4 @@
+#Day 58
 #When the constructor doesn't accept any arguments from the object and has only one argument, self, in the constructor, it is known as a Default constructor.
 
 class Details:
