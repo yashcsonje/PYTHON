5ef46b38f40b6800e1cd6119f7a4ad53a16c8b8b
yashcsonje <162028752+yashcsonje@users.noreply.github.com>
2024-04-20 17:33:39 +0530

    Add files via upload

---
+++ OOPs/Magic_Dunder.py
@@ -0,0 +1,6 @@
+#Day 73
+'''
+These are special methods that you can define in your classes, and when invoked, they give you a powerful way to manipulate objects and their behaviour.
+Magic methods, also known as “dunders” from the double underscores surrounding their names, are powerful tools that allow you to customize the behaviour of your classes. 
+They are used to implement special methods such as the addition, subtraction and comparison operators, as well as some more advanced techniques like descriptors and properties.
+'''
